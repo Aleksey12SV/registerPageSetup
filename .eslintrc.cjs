9bb1cd8445273243e685ff6b02e8d8f4164a6311
Aleksey12SV <113349663+Aleksey12SV@users.noreply.github.com>
2023-01-26 19:30:00 +0200

    feat: Added Setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    //        "eslint:recommended", // airbnb overwrites it
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect', // React version. "detect" automatically picks the version you have installed.
+    },
+  },
+};
